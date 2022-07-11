76fd404da3b78b82d427efa76dfd7cb181bc21b1
iury simões <iurysimoes_s@hotmail.com>
2022-07-11 17:34:37 -0300

    ok2

--- front-end/src/Service/utilServiceFrontEnd.js
+++ front-end/src/Service/utilServiceFrontEnd.js
@@ -7,6 +7,7 @@ import { cnpj } from "cpf-cnpj-validator";
 const emailV = /\S+@\S+\.\S+/;
 const funcaoTeste = "teste";
 const funcaoiury = "iury";
+const funcaoiury3 = "teste iury 3";
 
 //  const dataBRa = (data)=>{
 //     return(
